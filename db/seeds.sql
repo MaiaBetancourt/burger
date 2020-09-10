5a2ebef1361ed0f7fd98642d74f3ee8a854eb99f
Maria <maria.betancourt15@yahoo.com>
2020-09-10 10:01:55 -0700

    orm and config files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Sandwhich');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
